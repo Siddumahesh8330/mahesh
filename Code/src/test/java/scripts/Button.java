3e6bbbca5f6f3927450dc0d496ee68f551c07644
siddu <siddu@LAPTOP-T92788MC>
2022-01-23 12:57:05 +0530

    first project

---
+++ Code/src/test/java/scripts/Button.java
@@ -0,0 +1,30 @@
+package scripts;
+
+import org.apache.tools.ant.taskdefs.Sleep;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Button {
+	@Test
+	public void openbutton() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\siddu\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver b1;
+		b1=new ChromeDriver();
+		b1.get("http://leafground.com/");
+		b1.findElement(By.linkText("Button")).click();
+		b1.findElement(By.id("position")).click();
+		b1.findElement(By.id("home")).click();
+		b1.findElement(By.linkText("Button")).click();
+		b1.findElement(By.id("color")).click();
+		b1.findElement(By.id("home")).click();
+		b1.findElement(By.linkText("Button")).click();
+		b1.findElement(By.id("home")).click();
+		b1.findElement(By.linkText("Button")).click();
+		b1.findElement(By.id("size")).click();
+		b1.findElement(By.id("home")).click();
+		b1.close();
+       }
+
+}
